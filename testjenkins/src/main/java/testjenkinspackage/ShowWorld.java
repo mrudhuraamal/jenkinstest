2dd8cf160f8dc101bc0fa473ea08a4f75e425a52
amal <amal@LAPTOP-5SMNA8RP>
2024-11-15 11:51:20 +0530

    jenkins trial

---
+++ testjenkins/src/main/java/testjenkinspackage/ShowWorld.java
@@ -0,0 +1,14 @@
+package testjenkinspackage;
+
+public class ShowWorld {
+
+	public void show(){
+		System.out.println("hello world");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ShowWorld obj=new ShowWorld();
+		obj.show();
+	}
+
+}
